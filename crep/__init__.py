8bf075ce304319c227574e35369acb1815ffa722
vlaurent <vlaurent@eurobios.com>
2023-07-04 19:08:12 +0200

    [DEV] init

---
+++ crep/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2023 Eurobios
+# Licensed under the CeCILL License;
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     https://cecill.info/
+from crep.base import merge, compute_discontinuity, aggregate_constant
