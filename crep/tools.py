1124e429ae05af2d449cdf4ef3e324f11a720abf
Vincent Laurent <vincent.laurent@mews-labs.com>
2024-03-25 23:26:05 +0100

    [DEV] update admissible data frame

---
+++ crep/tools.py
@@ -0,0 +1,44 @@
+# Copyright 2023 Eurobios
+# Licensed under the CeCILL License;
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     https://cecill.info/
+import numpy as np
+import pandas as pd
+
+
+def get_overlapping(data: pd.DataFrame,
+                    id_discrete: iter,
+                    id_continuous: iter) -> pd.Series:
+    data_ = data.__deepcopy__()
+    data_ = data_.sort_values(by=[*id_discrete, id_continuous[0]])
+    data_["r1"] = range(len(data_))
+
+    condition = data_[id_continuous[1]].iloc[:-1].values >= data_[id_continuous[0]].iloc[1:].values
+    data_["local_overlap"] = False
+    data_.loc[data_.index[1:][condition], "local_overlap"] = True
+    for i in id_discrete:
+        values = data_[i]
+        condition = values.iloc[:-1].values == values.iloc[1:].values
+        condition = np.concatenate(([False], condition))
+        data_["local_overlap"] &= condition
+
+    data_ = data_.sort_values(by=[*id_discrete, id_continuous[1]])
+    data_["r2"] = range(len(data_))
+    data_ = data_.loc[data.index]
+    overlap = pd.Series(data_["r1"] != data_["r2"]) | data_["local_overlap"]
+    return overlap
+
+
+def admissible_dataframe(data: pd.DataFrame,
+                         id_discrete: iter,
+                         id_continuous: iter):
+    return sum(get_overlapping(data, id_discrete,
+                               id_continuous)) == 0
+
+
+def sample_non_admissible_data(data: pd.DataFrame,
+                               id_discrete: iter,
+                               id_continuous: iter):
+    return data[get_overlapping(data, id_discrete,
+                                id_continuous)]
---
+++ test/test_tools.py
@@ -0,0 +1,22 @@
+# Copyright 2023 Eurobios
+# Licensed under the CeCILL License;
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     https://cecill.info/
+
+from crep.tools import get_overlapping, admissible_dataframe
+
+__args__ = ["id"], ["t1", "t2"]
+def test_no_overlapping(get_examples):
+    assert sum(get_overlapping(
+        get_examples[0], ["id"], ["t1", "t2"]
+    )) == 0
+    assert admissible_dataframe(get_examples[0], *__args__)
+
+
+def test_overlapping(get_examples):
+    df = get_examples[0]
+    df.loc[1, "t1"] = 5
+    ret = get_overlapping(df, *__args__)
+    print(ret)
+    assert sum(ret) == 1
